2d40f7a28420982eec8a5374dc4306b672dd4265
Nikita Oltyan <76751355+nikitaoltyan@users.noreply.github.com>
2021-03-15 21:35:48 +0300

    Added findHighs function for easier implementation

--- classifiers/Triangle.py
+++ classifiers/Triangle.py
@@ -3,12 +3,22 @@ from HeadWithShoulders import head_with_shoulders
 
 
 class triangle:
+
+  def find_highs(self, data, rang):
+    """
+    That function finds local price highs on the given range: (prise-rang, price+rang)
+    Returns array with that highs.
+    """
+    lenth = len(data["<OPEN>"])
+    result = []
+    for day in range(rang, lenth-rang):
+      if data["<HIGH>"][day] == max(data["<HIGH>"][day-rang:day+rang]):
+        result.append(day)
+    return result
   
-  __init__(self):
-    self.hws = head_with_shoulders()
   
   def find_triangle(self, data, rang):
-    start_highs = self.hws.find_highs(data, rang)
+    start_highs = find_highs(data, rang)
     highs = list(data["<HIGH>"])
     lows = list(data["<LOW>"])
     result = []
