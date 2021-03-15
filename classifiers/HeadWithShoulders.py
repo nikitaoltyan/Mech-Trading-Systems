207f33bbabf98b835f1b0f5660d3234af2b8858e
Nikita Oltyan <76751355+nikitaoltyan@users.noreply.github.com>
2021-03-15 17:20:27 +0300

    Update HeadWithShoulders.py

--- classifiers/HeadWithShoulders.py
+++ classifiers/HeadWithShoulders.py
@@ -1,3 +1,40 @@
 import numpy as np
 
-Class 
+class HeadWithShoulders:
+
+  def find_highs(data, rang):
+    lenth = len(data["<OPEN>"])
+    result = []
+    for day in range(rang, lenth-rang):
+      if data["<HIGH>"][day] == max(data["<HIGH>"][day-rang:day+rang]):
+        result.append(day)
+    return result
+  
+  def find_lows(data, rang):
+    lenth = len(data["<OPEN>"])
+    result = []
+    for day in range(rang, lenth-rang):
+      if data["<LOW>"][day] == min(data["<LOW>"][day-rang:day+rang]):
+        result.append(day)
+    return result
+  
+  def head_with_shoulders(data, rang):
+    highs = data["<HIGH>"]
+    highs_indexes = find_highs(data=data, rang=rang)
+    lows = find_lows(data=data, rang=rang)
+    result = []
+    shoulders = []
+    for item in range(1, len(highs_indexes)-1):
+      head = highs[highs_indexes[item]]
+      leftShoulder = highs[highs_indexes[item-1]]
+      rightShoulder = highs[highs_indexes[item+1]]
+      # Head is upper then shoulders
+      meanE = 0
+      if leftShoulder<head>rightShoulder:
+        meanE = (leftShoulder + rightShoulder)/2
+      # Shoulders restriction
+      if meanE != 0:
+        if max([abs(leftShoulder-meanE), abs(rightShoulder-meanE)]) <= meanE*0.02:
+          result.append(highs_indexes[item])
+          shoulders.append((highs_indexes[item-1], highs_indexes[item+1]))
+    return result, shoulders
