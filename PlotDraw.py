0d54be624b586c47d584a2e70dc949f44ae7f781
Nikita Oltyan <76751355+nikitaoltyan@users.noreply.github.com>
2021-03-15 21:11:34 +0300

    Add comments

--- PlotDraw.py
+++ PlotDraw.py
@@ -1,6 +1,10 @@
 import plotly.graph_objects as go
 
-def draw_plot(data, start=0, end=len(data)):
+def draw_plot(data, start=0, end=1):
+  """
+  Function draws candle chart for given data: OPEN, HIGH, LOW, CLOSE is required.
+  Start and end are Int number for wanted data range chart.
+  """
   plot=[go.Candlestick(x=[i for i in range (0, len(data))][start:end],
           open=data['<OPEN>'][start:end],
           high=data['<HIGH>'][start:end],
