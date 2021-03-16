d6c331dcdaa94b6da200a6055b69e3df68928367
Nikita Oltyan <76751355+nikitaoltyan@users.noreply.github.com>
2021-03-16 20:41:50 +0300

    Update Triangle.py

--- classifiers/Triangle.py
+++ classifiers/Triangle.py
@@ -1,4 +1,21 @@
 import pandas as pd
+import plotly.graph_objects as go
+
+
+def draw_plot(data, start=0, end=1):
+  """
+  Function draws candle chart for given data: OPEN, HIGH, LOW, CLOSE is required.
+  Start and end are Int number for wanted data range chart.
+  """
+  plot=[go.Candlestick(x=[i for i in range (0, len(data))][start:end],
+          open=data['<OPEN>'][start:end],
+          high=data['<HIGH>'][start:end],
+          low=data['<LOW>'][start:end],
+          close=data['<CLOSE>'][start:end])]
+  figSignal = go.Figure(data=plot)
+  figSignal.show()
+  
+  
 
 def find_highs(data, rang):
   """
@@ -32,3 +49,8 @@ def find_triangle(data, rang):
       if (lows[ind_min]<highs[item]>highs[ind_high]) and (lows[ind_min]<highs[ind_high]):
         result.append((item, ind_min, ind_high))
   return result
+
+
+def draw_triangles(data, triangles):
+  for triangle in triangles:
+    draw_plot(data, triangle[0], triangle[-1])
