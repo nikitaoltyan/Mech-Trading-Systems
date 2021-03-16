a0f23737f0cc2ec18a8534a0d538ae0cb67ffede
Aliya <80745137+sonarway@users.noreply.github.com>
2021-03-16 17:24:51 +0300

    Update levels.py

--- levels.py
+++ levels.py
@@ -1,36 +1,38 @@
 import numpy as np
 
- def find_support(self, data):
-   """
-   finds support levels
-   returns: support - tuple(index, price)
-   """
-   support = []
-   supp_points = []
-   supp_close = []
+class Levels:
 
-   for i in range(1, data.shape[0]-1):
-     if data['<LOW>'][i] < data['<LOW>'][i-1] and data['<LOW>'][i] < data['<LOW>'][i+1] and data['<LOW>'][i+1] < data['<LOW>'][i+2] and data['<LOW>'][i-1] < data['<LOW>'][i-2]:
-       supp_points.append(i)
-       supp_close.append(data['<LOW>'][i])
-    
-   support = [(i,j) for i,j in zip(supp_points, supp_close)]
+  def find_support(self, data):
+    """
+    finds support levels
+    returns: support - tuple(index, price)
+    """
+    support = []
+    supp_points = []
+    supp_close = []
 
-   return support
+    for i in range(1, data.shape[0]-1):
+      if data['<LOW>'][i] < data['<LOW>'][i-1] and data['<LOW>'][i] < data['<LOW>'][i+1] and data['<LOW>'][i+1] < data['<LOW>'][i+2] and data['<LOW>'][i-1] < data['<LOW>'][i-2]:
+        supp_points.append(i)
+        supp_close.append(data['<LOW>'][i])
 
- def find_resistance(self, data):
-   """
-   finds resistance levels
-   returns: resistance - tuple(index,price)
-   """
-   resistance = []
-   res_points = []
-   res_close = []
+    support = [(i,j) for i,j in zip(supp_points, supp_close)]
 
-   for i in range(1, data.shape[0]-1):
-     if data['<HIGH>'][i] > data['<HIGH>'][i-1] and data['<HIGH>'][i] > data['<HIGH>'][i+1] and data['<HIGH>'][i+1] > data['<HIGH>'][i+2] and data['<HIGH>'][i-1] > data['<HIGH>'][i-2]:
-       res_points.append(i)
-       res_close.append(data['<HIGH>'][i])
+    return support
+
+  def find_resistance(self, data):
+    """
+    finds resistance levels
+    returns: resistance - tuple(index,price)
+    """
+    resistance = []
+    res_points = []
+    res_close = []
+
+    for i in range(1, data.shape[0]-1):
+      if data['<HIGH>'][i] > data['<HIGH>'][i-1] and data['<HIGH>'][i] > data['<HIGH>'][i+1] and data['<HIGH>'][i+1] > data['<HIGH>'][i+2] and data['<HIGH>'][i-1] > data['<HIGH>'][i-2]:
+        res_points.append(i)
+        res_close.append(data['<HIGH>'][i])
 
    resistance = [(i,j) for i,j in zip(res_points, res_close)]
 
