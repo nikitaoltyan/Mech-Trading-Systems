1ab3c88218a8697557af2651bed777a536094171
Nikita Oltyan <76751355+nikitaoltyan@users.noreply.github.com>
2021-03-16 20:39:55 +0300

    Update and rename Trand.py to Trend.py

--- classifiers/Trend.py
+++ classifiers/Trend.py
@@ -1,6 +1,21 @@
 import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
 
+
+def draw_plot(data, start=0, end=1):
+  """
+  Function draws candle chart for given data: OPEN, HIGH, LOW, CLOSE is required.
+  Start and end are Int number for wanted data range chart.
+  """
+  plot=[go.Candlestick(x=[i for i in range (0, len(data))][start:end],
+          open=data['<OPEN>'][start:end],
+          high=data['<HIGH>'][start:end],
+          low=data['<LOW>'][start:end],
+          close=data['<CLOSE>'][start:end])]
+  figSignal = go.Figure(data=plot)
+  figSignal.show()
+  
 
 def moving_average(data, moving_avg_range=5):
   avg_prices = (data["<HIGH>"]+data["<LOW>"])/2
@@ -10,7 +25,7 @@ def moving_average(data, moving_avg_range=5):
 
 
 
-def find_rising_trand_lines(data, moving_avg_range=5, trend_lenth = 10):
+def find_rising_trend_lines(data, moving_avg_range=5, trend_lenth = 10):
   moving_avg = moving_average(data, moving_avg_range)
   trends, current_trend, result = [], [], []
   for avg in moving_avg:
@@ -30,7 +45,7 @@ def find_rising_trand_lines(data, moving_avg_range=5, trend_lenth = 10):
 
 
 
-def find_decreasing_trand_lines(data, moving_avg_range=5, trend_lenth = 10):
+def find_decreasing_trend_lines(data, moving_avg_range=5, trend_lenth = 10):
   moving_avg = moving_average(data, moving_avg_range)
   trends, current_trend, result = [], [], []
   for avg in moving_avg:
@@ -47,3 +62,8 @@ def find_decreasing_trand_lines(data, moving_avg_range=5, trend_lenth = 10):
     if len(trend)>=trend_lenth:
       result.append(trend)
   return result
+
+
+def draw_trends(data, trends):
+    for trend in trends:
+      draw_plot(data, trend[0], trend[-1])
