7b06e566640e66f9664ab3e9c5bdf8e5bbd958c5
Nikita Oltyan <76751355+nikitaoltyan@users.noreply.github.com>
2021-06-03 10:42:15 +0300

    initial commit

---
+++ App/ODA_Trading_System/ODA Trading SystemTests/ODA_Trading_SystemTests.swift
@@ -0,0 +1,33 @@
+//
+//  ODA_Trading_SystemTests.swift
+//  ODA Trading SystemTests
+//
+//  Created by Никита Олтян on 19.05.2021.
+//
+
+import XCTest
+@testable import ODA_Trading_System
+
+class ODA_Trading_SystemTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
